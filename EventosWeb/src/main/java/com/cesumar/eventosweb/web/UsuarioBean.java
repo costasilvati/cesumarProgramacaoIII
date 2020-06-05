0e3abe9c060f66cb548cfb11d03924bfbfbc4796
Costa-SilvaTI <costasilvati@users.noreply.github.com>
2020-06-04 22:05:07 -0300

    Aula 04/06/2020
    página admin/principal

--- EventosWeb/src/main/java/com/cesumar/eventosweb/web/UsuarioBean.java
+++ EventosWeb/src/main/java/com/cesumar/eventosweb/web/UsuarioBean.java
@@ -2,6 +2,7 @@ package com.cesumar.eventosweb.web;
 
 import com.cesumar.eventosweb.usuario.Usuario;
 import com.cesumar.eventosweb.usuario.UsuarioRN;
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -13,8 +14,32 @@ import javax.inject.Named;
 public class UsuarioBean {
     private Usuario usuario = new Usuario();
     private String confirmarSenha;
+    private List<Usuario> lista;// 04/06/2020
+    private String destinoSalvar;// 04/06/2020
     
+    // 04/06/2020
+    public List<Usuario> getLista(){
+        if(this.lista == null){
+            UsuarioRN usuarioRN = new UsuarioRN();
+            this.lista = usuarioRN.listar();
+        }
+        return this.lista;
+    }
+    //04/06/2020
+    public String ativar(){
+        if(this.usuario.isAtivo()){
+            this.usuario.setAtivo(false);
+        }else{
+            this.usuario.setAtivo(true);
+        }
+        UsuarioRN usuarioRN = new UsuarioRN();
+        usuarioRN.salvar(this.usuario);
+        return null;// não sai da página atual
+    }
+    
+
     public String novo(){
+        this.destinoSalvar = "usuariosucesso";
         this.usuario = new Usuario();
         this.usuario.setAtivo(true);
         return "publico/usuario";
@@ -38,4 +63,7 @@ public class UsuarioBean {
     public void setUsuario(Usuario usuario) {this.usuario = usuario;}
     public String getConfirmarSenha() {return confirmarSenha;}
     public void setConfirmarSenha(String confirmarSenha) {this.confirmarSenha = confirmarSenha;}
+    public void setLista(List<Usuario> lista) {this.lista = lista;}
+    public String getDestinoSalvar() {return destinoSalvar;}
+    public void setDestinoSalvar(String destinoSalvar) {this.destinoSalvar = destinoSalvar;}
 }
